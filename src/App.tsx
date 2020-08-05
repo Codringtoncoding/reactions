4372d503386ab2d7f83f87b37769d4177f5ed257
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-08-05 15:20:33 +0100

    finished 2 part

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React, {FunctionComponent} from 'react';
 import {Game} from "./Game/Game";
 import styles from "./App.module.scss";
 
+
+
 export const App: FunctionComponent = () => {
+
     return (
         <main className={styles.main}>
             <Game/>
--- src/Game/Button/Button.tsx
+++ src/Game/Button/Button.tsx
@@ -1,14 +1,35 @@
-﻿import React, {FunctionComponent} from "react";
+﻿import React, { FunctionComponent } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
     id: number;
     x: number;
     y: number;
+    activeButtonId: number;
+    setActiveButtonId: (id: number) => void;
 }
 
-export const Button: FunctionComponent<ButtonProps> = ({id, x, y}) => {
+
+export const Button: FunctionComponent<ButtonProps> = ({ id, x, y, activeButtonId, setActiveButtonId }) => {
+
+    const isActive = id === activeButtonId;
+    const GetClassName = (): string => {
+        if (isActive) {
+            return `${styles.button} ${styles.active}`
+        }
+
+        return styles.button;
+
+    }
+
+    const buttonClicked = (): void => {
+        if (isActive) {
+            const randomButton = Math.floor(Math.random() * 8);
+            setActiveButtonId(randomButton);
+        }
+    }
+
     return (
-        <circle className={styles.button} cx={x} cy={y} r={100}/>
+        <circle onClick={buttonClicked} className={GetClassName()} cx={x} cy={y} r={100} />
     );
 };
